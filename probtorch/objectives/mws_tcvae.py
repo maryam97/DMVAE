aa2fc71789eee54c033dcf59da298946f8938496
Maryam Meghdadi <marmegh@gmail.com>
2023-10-30 16:57:47 +0100

    revert elbo

--- probtorch/objectives/mws_tcvae.py
+++ probtorch/objectives/mws_tcvae.py
@@ -1,27 +1,27 @@
-# def elbo(q, p, rec, latents=None, sample_dim=None, batch_dim=None, beta=[1.0,1.0,1.0],
-#          bias=1.0):
-#     reconst_loss = rec.loss
-#
-#     log_pz, log_qz, log_prod_qzi, log_q_zCx = _get_probability(q, p, latents, sample_dim, batch_dim, bias)
-#     kl = beta[0] * (log_q_zCx - log_qz) +  beta[1] * (log_qz - log_prod_qzi) + beta[2] * (log_prod_qzi - log_pz)
-#
-#     return (reconst_loss.mean(), kl.mean())
-
-import torch
-def elbo(q, p, rec, device, latents=None):  # , sample_dim=None, batch_dim=None, beta=[1.0,1.0,1.0],
+def elbo(q, p, rec, latents=None, sample_dim=None, batch_dim=None, beta=[1.0,1.0,1.0],
+         bias=1.0):
     reconst_loss = rec.loss
-    prior_s = p['priorSh'].dist
-    q_p, q_s = q[latents[0]].dist, q[latents[1]].dist
-    if 'privateA' in latents:
-        prior_p = p['priorA'].dist
-    else:
-        prior_p = p['priorB'].dist
 
-    kl_s = torch.distributions.kl.kl_divergence(q_s, prior_s).sum(-1).mean().to(device)
-    kl_p = torch.distributions.kl.kl_divergence(q_p, prior_p).sum(-1).mean().to(device)
-    kl = kl_p + kl_s
+    log_pz, log_qz, log_prod_qzi, log_q_zCx = _get_probability(q, p, latents, sample_dim, batch_dim, bias)
+    kl = beta[0] * (log_q_zCx - log_qz) +  beta[1] * (log_qz - log_prod_qzi) + beta[2] * (log_prod_qzi - log_pz)
 
-    return (reconst_loss, kl)
+    return (reconst_loss.mean(), kl.mean())
+
+# import torch
+# def elbo(q, p, rec, device, latents=None):  # , sample_dim=None, batch_dim=None, beta=[1.0,1.0,1.0],
+#     reconst_loss = rec.loss
+#     prior_s = p['priorSh'].dist
+#     q_p, q_s = q[latents[0]].dist, q[latents[1]].dist
+#     if 'privateA' in latents:
+#         prior_p = p['priorA'].dist
+#     else:
+#         prior_p = p['priorB'].dist
+#
+#     kl_s = torch.distributions.kl.kl_divergence(q_s, prior_s).sum(-1).mean().to(device)
+#     kl_p = torch.distributions.kl.kl_divergence(q_p, prior_p).sum(-1).mean().to(device)
+#     kl = kl_p + kl_s
+#
+#     return (reconst_loss, kl)
 
 
 def _get_probability(q, p, latents, sample_dim, batch_dim, bias):
